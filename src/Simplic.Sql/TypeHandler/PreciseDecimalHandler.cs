3655ddbf15d61543b44860fcfc6b2fd5cf2ca5f6
froeh010 <froeh010@uni-hildesheim.de>
2020-09-18 13:52:36 +0200

    Initial commit after moving from simplic-service

---
+++ src/Simplic.Sql/TypeHandler/PreciseDecimalHandler.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Simplic.Data;
+using System;
+using System.Data;
+
+namespace Simplic.Sql
+{
+    /// <summary>
+    /// <see cref="PreciseDecimal"/> dapper type handler
+    /// </summary>
+    internal class PreciseDecimalHandler : SqlMapper.TypeHandler<PreciseDecimal>
+    {
+        public override void SetValue(IDbDataParameter parameter, PreciseDecimal value)
+        {
+            parameter.Value = (double)value;
+        }
+
+        public override PreciseDecimal Parse(object value)
+        {
+            if(value is decimal)
+            {
+                return new PreciseDecimal(Convert.ToDouble((decimal)value));
+            }
+            return new PreciseDecimal((double)value);
+        }
+    }
+}
